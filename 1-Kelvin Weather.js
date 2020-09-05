a09a25804bb2f9940fb536658f286dc3c87f291a
Jatin sharma <jatinsharma089659@gmail.com>
2020-09-05 22:39:22 +0530

    Create 1-Kelvin Weather.js

---
+++ 1-Kelvin Weather.js
@@ -0,0 +1,12 @@
+//link : https://www.codecademy.com/courses/introduction-to-javascript/projects/kelvin-weather-javascript
+
+//variable for kelvin
+let kelvin = 293;
+//tem - kelvin
+let celsius = kelvin - 273;
+//formula
+let fahrenheit = celsius*(9/5) +32;
+
+result = Math.floor(fahrenheit)
+
+console.log(`The temperature is ${result} degrees Fahrenheit`)
